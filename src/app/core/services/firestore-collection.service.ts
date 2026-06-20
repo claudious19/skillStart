d4df2b9b8e8ace7b2db8a68cc5b8b65b4b48d441
claudious19 <claudiotopolino05@gmail.com>
2026-06-20 12:43:55 +0200

    commit before pull

---
+++ src/app/core/services/firestore-collection.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, inject } from '@angular/core';
+import { CollectionReference, DocumentData, DocumentReference, collection, doc } from 'firebase/firestore';
+
+import { FirestoreCollectionName } from '../../firebase/firestore.collections';
+import { FIREBASE_FIRESTORE } from '../../firebase/firebase.tokens';
+
+@Injectable({ providedIn: 'root' })
+export class FirestoreCollectionService {
+  readonly firestore = inject(FIREBASE_FIRESTORE);
+
+  collection<T extends DocumentData>(collectionName: FirestoreCollectionName): CollectionReference<T> {
+    return collection(this.firestore, collectionName) as CollectionReference<T>;
+  }
+
+  doc<T extends DocumentData>(
+    collectionName: FirestoreCollectionName,
+    documentId: string,
+  ): DocumentReference<T> {
+    return doc(this.firestore, collectionName, documentId) as DocumentReference<T>;
+  }
+}
